b06806f60a2b08642da26c6ef8e11aaba66858b9
MichalDawidowicz <mpd2001pl@gmail.com>
2023-03-21 16:42:05 +0100

    Commit 1 :)

--- projekt1/src/InOut.java
+++ projekt1/src/InOut.java
@@ -3,7 +3,9 @@ public class InOut {
     static public void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int a,b,c;
+        System.out.print("Podaj 1 liczbe:");
         a=scanner.nextInt();
+        System.out.print("Podaj 2 liczbe:");
         b= scanner.nextInt();
         c=a+b;
         System.out.println("Suma = " + c);
