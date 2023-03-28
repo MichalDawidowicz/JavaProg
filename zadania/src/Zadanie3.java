945e2678c40222bb156daf988cc7228d2ec82e7f
MichalDawidowicz <mpd2001pl@gmail.com>
2023-03-28 16:48:44 +0200

    :DD

---
+++ zadania/src/Zadanie3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków:");
+        String text = scanner.next();
+    }
+}
